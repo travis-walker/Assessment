7a472f33d6a2cdd2480e827951044473ad1b3653
Travis Walker <travisw@nevada.unr.edu>
2021-09-06 18:36:57 -0700

    finalized file name and state code validation. df now gets filtered by state code

--- analyze_elevation_by_state.py
+++ analyze_elevation_by_state.py
@@ -11,20 +11,33 @@ def is_valid_state_code(code):
 
 
 def load_data_from_file(file_name):
+    df = pd.DataFrame()
     try:
         df = pd.read_csv(file_name)
-        return df
     except FileNotFoundError:
         print("File name is invalid. Please check and try again.")
+    return df
+
+
+def calculate_statistics(df):
+    print(df.tail())
+    print(df.size)
 
 
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) == 2:
         data_file_name, state_code = args
+        state_code = state_code.upper()
         if is_valid_state_code(state_code):
             df_all = load_data_from_file(data_file_name)
+            if not df_all.empty:
+                df_state = df_all[df_all.state == state_code]
+                if df_state.size > 0:
+                    calculate_statistics(df_state)
+                else:
+                    print("State code is valid, but no data exists for it in file.")
         else:
-            print("State code is invalid. Please check and try again")
+            print("State code is invalid. Please check and try again.")
     else:
         print("Args are <data file name> <state code>")
