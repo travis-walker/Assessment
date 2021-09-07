3076488f921c1fd0968763bd9911918f378e8ee4
Travis Walker <travisw@nevada.unr.edu>
2021-09-06 18:12:46 -0700

    added pandas and function for loading data from csv file

--- analyze_elevation_by_state.py
+++ analyze_elevation_by_state.py
@@ -1,5 +1,7 @@
 import sys
 
+import pandas as pd
+
 
 def is_valid_state_code(code):
     return code in ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA",
@@ -8,12 +10,21 @@ def is_valid_state_code(code):
                     "WV", "WI", "WY"]
 
 
+def load_data_from_file(file_name):
+    try:
+        df = pd.read_csv(file_name)
+        return df
+    except FileNotFoundError:
+        print("File name is invalid. Please check and try again.")
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) == 2:
         data_file_name, state_code = args
-        print(data_file_name)
-        print(state_code)
-        print(is_valid_state_code(state_code))
+        if is_valid_state_code(state_code):
+            df_all = load_data_from_file(data_file_name)
+        else:
+            print("State code is invalid. Please check and try again")
     else:
         print("Args are <data file name> <state code>")
